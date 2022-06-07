5be56ca757c5dae41b7fd4917c8f7e9e819c37a5
vladimirtarasov <vptarasov1998@gmail.com>
2022-06-07 17:40:29 +0300

    Implement multiple choice in black number list screen

--- app/src/main/java/com/tarasovvp/blacklister/ui/main/blacknumberlist/BlackNumberListFragment.kt
+++ app/src/main/java/com/tarasovvp/blacklister/ui/main/blacknumberlist/BlackNumberListFragment.kt
@@ -5,12 +5,14 @@ import androidx.navigation.fragment.findNavController
 import com.tarasovvp.blacklister.R
 import com.tarasovvp.blacklister.constants.Constants.BLACK_NUMBER
 import com.tarasovvp.blacklister.databinding.FragmentBlackNumberListBinding
+import com.tarasovvp.blacklister.extensions.isTrue
 import com.tarasovvp.blacklister.extensions.safeObserve
 import com.tarasovvp.blacklister.extensions.safeSingleObserve
 import com.tarasovvp.blacklister.extensions.showPopUpMenu
 import com.tarasovvp.blacklister.model.BlackNumber
 import com.tarasovvp.blacklister.ui.base.BaseAdapter
 import com.tarasovvp.blacklister.ui.base.BaseListFragment
+import com.tarasovvp.blacklister.utils.MultipleChoiceSpinner
 import com.tarasovvp.blacklister.utils.setSafeOnClickListener
 import java.util.*
 
@@ -48,6 +50,15 @@ class BlackNumberListFragment :
         swipeRefresh = binding?.blackNumberListRefresh
         recyclerView = binding?.blackNumberListRecyclerView
         emptyListText = binding?.blackNumberListEmpty
+        binding?.blackNumberListSearch?.setItems(listOf(getString(R.string.black_number_contain),
+            getString(R.string.black_number_start),
+            getString(R.string.black_number_end)), getString(
+            R.string.black_number_no_filter), object : MultipleChoiceSpinner.MultiSpinnerListener {
+            override fun onItemsSelected(selected: BooleanArray) {
+                searchDataList()
+            }
+
+        })
         findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<BlackNumber>(
             BLACK_NUMBER
         )
@@ -83,10 +94,19 @@ class BlackNumberListFragment :
         val filteredBlackNumberList = blackNumberList?.filter { blackNumber ->
             blackNumber.blackNumber.lowercase(Locale.getDefault()).contains(
                 searchQuery?.lowercase(Locale.getDefault()).orEmpty()
-            )
+            ) && (if (binding?.blackNumberListSearch?.selected?.get(0)
+                    .isTrue()
+            ) blackNumber.isContain else true)
+                    && (if (binding?.blackNumberListSearch?.selected?.get(1)
+                    .isTrue()
+            ) blackNumber.isStart else true)
+                    && if (binding?.blackNumberListSearch?.selected?.get(2)
+                    .isTrue()
+            ) blackNumber.isEnd else true
         } as ArrayList<BlackNumber>
         if (!checkDataListEmptiness(filteredBlackNumberList)) {
             viewModel.getHashMapFromBlackNumberList(filteredBlackNumberList)
         }
     }
 }
+
