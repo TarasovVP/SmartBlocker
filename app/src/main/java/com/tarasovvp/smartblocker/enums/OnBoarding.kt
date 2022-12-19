57d372307386fea494e632484f2e18e43d6677eb
vladimirtarasov <vptarasov1998@gmail.com>
2022-12-19 20:04:59 +0200

    Fix onboarding

--- app/src/main/java/com/tarasovvp/smartblocker/enums/OnBoarding.kt
+++ app/src/main/java/com/tarasovvp/smartblocker/enums/OnBoarding.kt
@@ -1,18 +1,25 @@
 package com.tarasovvp.smartblocker.enums
 
+import android.content.Context
+import android.text.Spanned
 import com.tarasovvp.smartblocker.R
+import com.tarasovvp.smartblocker.extensions.htmlWithImages
 
 enum class OnBoarding(val description: Int, val mainImage: Int, val tabImage: Int) {
-    INNER_DATA_ACCESS(R.string.inner_data_access_title,
+    INNER_DATA_ACCESS(R.string.onboarding_intro,
         R.drawable.ic_inner_data_access,
         R.drawable.ic_tab_first),
-    BLACK_WHITE_LIST(R.string.black_white_list_title,
+    BLACK_WHITE_LIST(R.string.onboarding_filter_conditions,
         R.drawable.ic_black_white_list,
         R.drawable.ic_tab_second),
-    AUTHORIZED_USER(R.string.authorized_user_title,
+    AUTHORIZED_USER(R.string.onboarding_filter_types,
         R.drawable.ic_authorized_user,
         R.drawable.ic_tab_third),
-    ACCESS_PERMISSIONS(R.string.access_permissions,
+    ACCESS_PERMISSIONS(R.string.onboarding_permissions,
         R.drawable.ic_access_permissions,
-        R.drawable.ic_tab_last)
+        R.drawable.ic_tab_last);
+
+    fun getHtmlWithImages(context: Context): Spanned {
+        return context.htmlWithImages(context.getString(description))
+    }
 }
