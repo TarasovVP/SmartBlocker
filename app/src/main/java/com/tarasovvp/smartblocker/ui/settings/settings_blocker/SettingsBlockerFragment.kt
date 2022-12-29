c6375e8bfb8921ff36a8bc00de43f3ed088cb024
vladimirtarasov <vptarasov1998@gmail.com>
2022-12-29 17:30:29 +0200

    Change settings blocker screen

--- app/src/main/java/com/tarasovvp/smartblocker/ui/settings/settings_blocker/SettingsBlockerFragment.kt
+++ app/src/main/java/com/tarasovvp/smartblocker/ui/settings/settings_blocker/SettingsBlockerFragment.kt
@@ -2,23 +2,18 @@ package com.tarasovvp.smartblocker.ui.settings.settings_blocker
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.setFragmentResultListener
-import androidx.navigation.fragment.findNavController
 import com.tarasovvp.smartblocker.BlackListerApp
 import com.tarasovvp.smartblocker.R
-import com.tarasovvp.smartblocker.constants.Constants
 import com.tarasovvp.smartblocker.databinding.FragmentSettingsBlockerBinding
 import com.tarasovvp.smartblocker.extensions.isNotTrue
 import com.tarasovvp.smartblocker.extensions.isTrue
-import com.tarasovvp.smartblocker.extensions.parcelable
 import com.tarasovvp.smartblocker.extensions.safeSingleObserve
 import com.tarasovvp.smartblocker.local.SharedPreferencesUtil
-import com.tarasovvp.smartblocker.models.CountryCode
 import com.tarasovvp.smartblocker.ui.MainActivity
 import com.tarasovvp.smartblocker.ui.base.BaseFragment
-import com.tarasovvp.smartblocker.utils.setSafeOnClickListener
 
-class SettingsBlockerFragment : BaseFragment<FragmentSettingsBlockerBinding, SettingsBlockerViewModel>() {
+class SettingsBlockerFragment :
+    BaseFragment<FragmentSettingsBlockerBinding, SettingsBlockerViewModel>() {
 
     override var layoutId = R.layout.fragment_settings_blocker
     override val viewModelClass = SettingsBlockerViewModel::class.java
@@ -31,21 +26,20 @@ class SettingsBlockerFragment : BaseFragment<FragmentSettingsBlockerBinding, Set
     private fun initViews() {
         binding?.apply {
             (activity as MainActivity).apply {
-                blockSettingsTurn.setSwitchChange(SharedPreferencesUtil.blockTurnOff.not())
-                checkBlockSettingsEnable(SharedPreferencesUtil.blockTurnOff.not())
-                blockSettingsTurn.setSwitchClickListener { isChecked ->
-                    blockSettingsTurn.setSwitchChange(isChecked.not())
+                settingsBlockerSwitch.isChecked = SharedPreferencesUtil.blockTurnOff.not()
+                settingsBlockerSwitch.setOnCheckedChangeListener { _, isChecked ->
                     SharedPreferencesUtil.blockTurnOff = isChecked
-                    if (isChecked.not()) {
-                        startBlocker()
-                    } else {
-                        stopBlocker()
+                    (activity as MainActivity).apply {
+                        if (isChecked.not()) {
+                            startBlocker()
+                        } else {
+                            stopBlocker()
+                        }
                     }
-                    checkBlockSettingsEnable(isChecked.not())
                 }
             }
-            blockSettingsHidden.setSwitchChange(SharedPreferencesUtil.blockHidden)
-            blockSettingsHidden.setSwitchClickListener { isChecked ->
+            settingsBlockerHiddenSwitch.isChecked = SharedPreferencesUtil.blockHidden
+            settingsBlockerHiddenSwitch.setOnCheckedChangeListener { _, isChecked ->
                 if (BlackListerApp.instance?.isLoggedInUser()
                         .isTrue() && BlackListerApp.instance?.isNetworkAvailable.isNotTrue()
                 ) {
@@ -54,32 +48,13 @@ class SettingsBlockerFragment : BaseFragment<FragmentSettingsBlockerBinding, Set
                     viewModel.changeBlockHidden(isChecked.not())
                 }
             }
-            viewModel.getCountryCodeWithCountry(SharedPreferencesUtil.countryCode)
-            settingsCountryCodeSpinner.setSafeOnClickListener {
-                findNavController().navigate(SettingsBlockerFragmentDirections.startCountryCodeSearchDialog())
-            }
-            setFragmentResultListener(Constants.COUNTRY_CODE) { _, bundle ->
-                bundle.parcelable<CountryCode>(Constants.COUNTRY_CODE)?.let { viewModel.countryCodeLiveData.postValue(it) }
-            }
-        }
-    }
-
-    private fun checkBlockSettingsEnable(isChecked: Boolean) {
-        binding?.apply {
-            blockSettingsHidden.setEnableChange(isChecked)
         }
     }
 
     override fun observeLiveData() {
         with(viewModel) {
             successBlockHiddenLiveData.safeSingleObserve(viewLifecycleOwner) { blockHidden ->
-                binding?.blockSettingsHidden?.setSwitchChange(blockHidden)
                 SharedPreferencesUtil.blockHidden = blockHidden
-                (activity as MainActivity).getAllData()
-            }
-            countryCodeLiveData.safeSingleObserve(viewLifecycleOwner) { countryCode ->
-                SharedPreferencesUtil.countryCode = countryCode.country
-                binding?.settingsCountryCodeSpinner?.text = countryCode.countryEmoji()
             }
         }
     }
