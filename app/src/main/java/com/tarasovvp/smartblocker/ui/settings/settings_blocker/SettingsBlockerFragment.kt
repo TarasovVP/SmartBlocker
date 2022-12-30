7c6602cefbac75f5fe7ab1558594f9a78a8ae5cd
vladimirtarasov <vptarasov1998@gmail.com>
2022-12-30 14:05:38 +0200

    Improve switch view in settings blocker screen

--- app/src/main/java/com/tarasovvp/smartblocker/ui/settings/settings_blocker/SettingsBlockerFragment.kt
+++ app/src/main/java/com/tarasovvp/smartblocker/ui/settings/settings_blocker/SettingsBlockerFragment.kt
@@ -20,26 +20,38 @@ class SettingsBlockerFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews()
+        setBlockerSettings()
+        setBlockHiddenSettings()
     }
 
-    private fun initViews() {
+    private fun setBlockerSettings() {
         binding?.apply {
-            (activity as MainActivity).apply {
-                settingsBlockerSwitch.isChecked = SharedPreferencesUtil.blockTurnOff.not()
-                settingsBlockerSwitch.setOnCheckedChangeListener { _, isChecked ->
-                    SharedPreferencesUtil.blockTurnOff = isChecked
-                    (activity as MainActivity).apply {
-                        if (isChecked.not()) {
-                            startBlocker()
-                        } else {
-                            stopBlocker()
-                        }
+            settingsBlockerSwitch.isChecked = SharedPreferencesUtil.blockTurnOff.not()
+            settingsBlockerDescribe.text =
+                getString(if (settingsBlockerSwitch.isChecked) R.string.settings_blocker_on else R.string.settings_blocker_off)
+            settingsBlockerSwitch.setOnCheckedChangeListener { _, isChecked ->
+                SharedPreferencesUtil.blockTurnOff = isChecked
+                settingsBlockerDescribe.text =
+                    getString(if (isChecked) R.string.settings_blocker_on else R.string.settings_blocker_off)
+                (activity as MainActivity).apply {
+                    if (isChecked) {
+                        startBlocker()
+                    } else {
+                        stopBlocker()
                     }
                 }
             }
+        }
+    }
+
+    private fun setBlockHiddenSettings() {
+        binding?.apply {
             settingsBlockerHiddenSwitch.isChecked = SharedPreferencesUtil.blockHidden
+            settingsBlockerHiddenDescribe.text =
+                getString(if (settingsBlockerHiddenSwitch.isChecked) R.string.settings_block_hidden_on else R.string.settings_block_hidden_off)
             settingsBlockerHiddenSwitch.setOnCheckedChangeListener { compoundButton, isChecked ->
+                settingsBlockerHiddenDescribe.text =
+                    getString(if (isChecked) R.string.settings_block_hidden_on else R.string.settings_block_hidden_off)
                 if (compoundButton.isPressed) {
                     if (BlackListerApp.instance?.isLoggedInUser()
                             .isTrue() && BlackListerApp.instance?.isNetworkAvailable.isNotTrue()
@@ -47,6 +59,7 @@ class SettingsBlockerFragment :
                         showMessage(getString(R.string.unavailable_network_repeat), true)
                     } else {
                         viewModel.changeBlockHidden(isChecked.not())
+                        binding?.settingsBlockerHiddenSwitch?.isEnabled = false
                     }
                 }
             }
@@ -56,11 +69,14 @@ class SettingsBlockerFragment :
     override fun observeLiveData() {
         with(viewModel) {
             successBlockHiddenLiveData.safeSingleObserve(viewLifecycleOwner) { blockHidden ->
+                binding?.settingsBlockerHiddenSwitch?.isEnabled = true
                 binding?.settingsBlockerHiddenSwitch?.isChecked = blockHidden.not()
                 SharedPreferencesUtil.blockHidden = blockHidden
             }
             exceptionLiveData.safeSingleObserve(viewLifecycleOwner) { error ->
-                binding?.settingsBlockerHiddenSwitch?.isChecked = binding?.settingsBlockerHiddenSwitch?.isChecked.isTrue().not()
+                binding?.settingsBlockerHiddenSwitch?.isEnabled = true
+                binding?.settingsBlockerHiddenSwitch?.isChecked =
+                    binding?.settingsBlockerHiddenSwitch?.isChecked.isTrue().not()
                 showMessage(error, true)
             }
         }
