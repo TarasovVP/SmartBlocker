ef2449867f3bdfcf8bfa2156e49e022b3119428d
vladimirtarasov <vptarasov1998@gmail.com>
2023-04-29 19:32:25 +0300

    Implement mockk in log call repository tests

--- app/src/test/java/com/tarasovvp/smartblocker/repositories/LogCallRepositoryTest.kt
+++ app/src/test/java/com/tarasovvp/smartblocker/repositories/LogCallRepositoryTest.kt
@@ -4,7 +4,6 @@ import android.content.ContentResolver
 import android.content.Context
 import android.database.Cursor
 import android.os.Build
-import com.nhaarman.mockitokotlin2.*
 import com.tarasovvp.smartblocker.UnitTestUtils.TEST_FILTER
 import com.tarasovvp.smartblocker.UnitTestUtils.TEST_NUMBER
 import com.tarasovvp.smartblocker.data.database.dao.LogCallDao
@@ -22,18 +21,13 @@ import com.tarasovvp.smartblocker.infrastructure.constants.Constants
 import com.tarasovvp.smartblocker.infrastructure.constants.Constants.BLOCKED_CALL
 import com.tarasovvp.smartblocker.utils.extensions.EMPTY
 import com.tarasovvp.smartblocker.utils.extensions.isTrue
-import io.mockk.MockKAnnotations
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.MockitoAnnotations
-import org.mockito.junit.MockitoJUnitRunner
 
 @ExperimentalCoroutinesApi
 class LogCallRepositoryTest {
@@ -50,7 +44,7 @@ class LogCallRepositoryTest {
     }
 
     @Test
-    fun setFilterToLogCallTest() = runTest {
+    fun setFilterToLogCallTest() = runBlocking {
         val filterList = listOf(
             Filter("1234567890", conditionType = FilterCondition.FILTER_CONDITION_FULL.index),
             Filter("345", conditionType = FilterCondition.FILTER_CONDITION_START.index),
@@ -62,103 +56,92 @@ class LogCallRepositoryTest {
             LogCall().apply { number = "5678901234" }
         )
 
-        val resultMock = mock<(Int, Int) -> Unit>()
+        val resultMock = mockk<(Int, Int) -> Unit>(relaxed = true)
         val modifiedContacts = logCallRepository.setFilterToLogCall(filterList, logCallList, resultMock)
 
         assertEquals("1234567890", modifiedContacts[0].filter)
         assertEquals("345", modifiedContacts[1].filter)
         assertEquals("789", modifiedContacts[2].filter)
-        verify(resultMock, times(logCallList.size)).invoke(eq(logCallList.size), any())
+        verify(exactly = logCallList.size) { resultMock.invoke(any(), any()) }
     }
 
     @Test
-    fun insertAllLogCallsTest() = runTest {
+    fun insertAllLogCallsTest() = runBlocking {
         val logCallList = listOf(LogCall().apply { number = TEST_NUMBER }, LogCall())
+        coEvery { logCallDao.insertAllLogCalls(logCallList) } just Runs
         logCallRepository.insertAllLogCalls(logCallList)
-        verify(logCallDao, times(1)).insertAllLogCalls(logCallList)
+        coVerify(exactly = 1) { logCallDao.insertAllLogCalls(logCallList) }
     }
 
     @Test
-    fun getAllLogCallWithFilterTest() = runTest {
+    fun getAllLogCallWithFilterTest() = runBlocking {
         val logCallList = listOf(LogCallWithFilter().apply { call = LogCall(callId = 1) }, LogCallWithFilter().apply { call = LogCall(callId = 2) })
-        `when`(logCallDao.allLogCallWithFilter())
-            .thenReturn(logCallList)
+        coEvery { logCallDao.allLogCallWithFilter() } returns logCallList
         val result = logCallRepository.getAllLogCallWithFilter()
         assertEquals(logCallList, result)
     }
 
     @Test
-    fun allCallNumberWithFilterTest() = runTest {
+    fun allCallNumberWithFilterTest() = runBlocking {
         val logCallList = listOf(LogCallWithFilter().apply { call = LogCall(callId = 1).apply { number = "1" } }, LogCallWithFilter().apply { call = LogCall(callId = 2).apply { number = "1" } }, LogCallWithFilter().apply { call = LogCall(callId = 3).apply { number = "2" }})
-        `when`(logCallDao.allCallNumberWithFilter())
-            .thenReturn(logCallList)
+        coEvery { logCallDao.allCallNumberWithFilter() } returns logCallList
         val result = logCallRepository.allCallNumberWithFilter()
         assertEquals(logCallList.distinctBy { it.call?.number }, result)
     }
 
     @Test
-    fun getLogCallWithFilterByFilterTest() = runTest {
+    fun getLogCallWithFilterByFilterTest() = runBlocking {
         val logCallList = listOf(LogCallWithFilter().apply { call = LogCall(callId = 1).apply { number = "1" } }, LogCallWithFilter().apply { call = LogCall(callId = 2).apply { number = "1" } }, LogCallWithFilter().apply { call = LogCall(callId = 3).apply { number = "2" }})
-        `when`(logCallDao.queryCallList(TEST_FILTER))
-            .thenReturn(logCallList)
+        coEvery { logCallDao.queryCallList(TEST_FILTER) } returns logCallList
         val result = logCallRepository.getLogCallWithFilterByFilter(TEST_FILTER)
         assertEquals(logCallList.distinctBy { it.call?.number }, result)
     }
 
     @Test
-    fun getSystemLogCallListTest() = runTest {
+    fun getSystemLogCallListTest() = runBlocking {
         val logCall = LogCall()
-        val context = mock<Context>()
-        val contentResolver = mock<ContentResolver>()
-        val cursor = mock<Cursor>()
-        val resultMock = mock<(Int, Int) -> Unit>()
+        val context = mockk<Context>()
+        val contentResolver = mockk<ContentResolver>()
+        val cursor = mockk<Cursor>()
+        val resultMock = mockk<(Int, Int) -> Unit>(relaxed = true)
         val expectedSize = 10
 
-        `when`(context.contentResolver).thenReturn(contentResolver)
-        `when`(
-            contentResolver.query(
-                anyOrNull(),
-                anyOrNull(),
-                anyOrNull(),
-                anyOrNull(),
-                anyOrNull()
-            )
-        ).thenReturn(cursor)
-
-        `when`(cursor.count).thenReturn(expectedSize)
+        every { context.contentResolver } returns contentResolver
+        every { contentResolver.query(any(), any(), any(), any(), any()) } returns cursor
+        every { cursor.count } returns expectedSize
         var index = 0
-        `when`(cursor.moveToNext()).thenAnswer {
+        every { cursor.moveToNext() } answers {
             index++ < expectedSize
         }
-        `when`(cursor.getInt(0)).thenReturn(logCall.callId)
-        `when`(cursor.getString(1)).thenReturn(logCall.callName)
-        `when`(cursor.getString(2)).thenReturn(logCall.number)
-        `when`(cursor.getString(3)).thenReturn(logCall.type)
-        `when`(cursor.getString(4)).thenReturn(logCall.callDate)
-        `when`(cursor.getString(5)).thenReturn(logCall.normalizedNumber)
-        `when`(cursor.getString(6)).thenReturn(logCall.countryIso)
+        every { cursor.getInt(0) } returns logCall.callId
+        every { cursor.getString(1) } returns logCall.callName
+        every { cursor.getString(2) } returns logCall.number
+        every { cursor.getString(3) } returns logCall.type
+        every { cursor.getString(4) } returns logCall.callDate
+        every { cursor.getString(5) } returns logCall.normalizedNumber
+        every { cursor.getString(6) } returns logCall.countryIso
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            `when`(cursor.getString(7)).thenReturn(logCall.photoUrl)
+            every { cursor.getString(7) } returns logCall.photoUrl
         }
+        every { cursor.close() } just Runs
 
         val contactList = logCallRepository.getSystemLogCallList(context, resultMock)
 
         assertEquals(expectedSize, contactList.size)
-        verify(resultMock, times(expectedSize)).invoke(eq(expectedSize), any())
-
-        verify(context).contentResolver
-        verify(cursor).close()
+        verify(exactly = expectedSize) { resultMock.invoke(any(), any()) }
+        verify(exactly = 1) { context.contentResolver }
+        verify(exactly = 1) { cursor.close() }
     }
 
     @Test
-    fun getFilteredCallListTest() = runTest {
+    fun getFilteredCallListTest() = runBlocking {
         val callList = listOf(CallWithFilter(call = FilteredCall().apply { type = BLOCKED_CALL }, filterWithCountryCode = FilterWithCountryCode(filter = Filter(filterType = Constants.BLOCKER))), CallWithFilter(call = LogCall().apply { number = "567" }))
         val result = logCallRepository.getFilteredCallList(callList, String.EMPTY, arrayListOf(NumberDataFiltering.CALL_BLOCKED.ordinal))
         assertEquals(callList.filter { it.filterWithCountryCode?.filter?.isBlocker().isTrue() }, result)
     }
 
     @Test
-    fun getHashMapFromCallListTest() = runTest {
+    fun getHashMapFromCallListTest() = runBlocking {
         val logCallList = listOf(LogCallWithFilter().apply { call = LogCall(callId = 1).apply { number = "1" } }, LogCallWithFilter().apply { call = LogCall(callId = 2).apply { number = "1" } }, LogCallWithFilter().apply { call = LogCall(callId = 3).apply { number = "2" }})
         val result = logCallRepository.getHashMapFromCallList(logCallList)
         assertEquals(logCallList.groupBy { it.call?.dateFromCallDate() }, result)
