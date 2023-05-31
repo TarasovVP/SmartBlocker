23cad58391c5ce923877a025887c5f7abbdb4f63
vladimirtarasov <vptarasov1998@gmail.com>
2023-05-31 18:26:20 +0300

    Implement success robolectric info screen unit test for release build

---
+++ app/src/release/java/com/tarasovvp/smartblocker/EmptyActivity.kt
@@ -0,0 +1,7 @@
+package com.tarasovvp.smartblocker
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EmptyActivity: AppCompatActivity()
